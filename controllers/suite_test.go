1dd35f7910d27286ce30be24691b44f24cc0702f
Andrea Fasano <afasano@redhat.com>
2024-05-21 13:13:02 -0400

    minor code fixes after dependencies bump

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -26,7 +26,7 @@ import (
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -44,11 +44,11 @@ var testEnv *envtest.Environment
 func TestAPIs(t *testing.T) {
 	t.Skip()
 
-	RegisterFailHandler(Fail)
+	// RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	// RunSpecsWithDefaultAndCustomReporters(t,
+	// 	"Controller Suite",
+	// 	[]Reporter{printer.NewlineReporter{}})
 }
 
 var _ = BeforeSuite(func() {
